0ebec4d7aedfe8e88e41bf5939bf300065a83a83
Mehmet Emin <mteminn@gmail.com>
2024-07-09 14:22:12 +0300

    project name changed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'proverbiom' title`, () => {
+  it(`should have the 'quotiom' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('proverbiom');
+    expect(app.title).toEqual('quotiom');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, proverbiom');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, quotiom');
   });
 });
