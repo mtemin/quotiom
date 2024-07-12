5e256991e0118d3064c6f452bfb5b38109714dd6
Mehmet Emin <mteminn@gmail.com>
2024-07-12 16:13:53 +0300

    delete kebab-case components to create new PascalCase ones

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,16 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
 import topics from "../../mockDB/topics";
-import quoteSource from "../../mockDB/quoteSource";
+import categories from "../../mockDB/categories";
 import famousPeople from "../../mockDB/famousPeople";
-import {NavbarComponent} from "../../components/navbar/navbar.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     NgOptimizedImage,
-    NavbarComponent,
     // topics,
-    // quoteSource,
+    // categories,
     // famousPeople,
 
   ],
