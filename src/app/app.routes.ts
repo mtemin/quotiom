fc06680e5ca6e609f99f969cc66e91bdd898ca94
Mehmet Emin <mteminn@gmail.com>
2024-07-12 16:38:55 +0300

    added categories route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import {CategoriesComponent} from "./pages/categories/categories.component";
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     title: 'Quotium | Home Page'
+  },
+  {
+    path: 'categories',
+    component: CategoriesComponent,
+    title: 'Quote Categories'
   },
 ];
