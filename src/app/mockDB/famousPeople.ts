88727c233a2218dbd99a2f91f8272dc9bb1e050c
Mehmet Emin <mteminn@gmail.com>
2024-07-13 18:02:55 +0300

    convert string array to object array for people with aliases(optional)

--- src/app/mockDB/famousPeople.ts
+++ src/app/mockDB/famousPeople.ts
@@ -1,12 +1,39 @@
-const famousPeople:string[] = [
-  "Alfred Adler",
-  "Necip Fazıl Kısakürek",
-  "Marcus Aurelius",
-  "Arthur Schopenhauer",
-  "Friedrich Nietzsche",
-  "Napoleon",
-  "İbn Haldun",
-  "Cristiano Ronaldo",
-  "Muammar al-Gaddafi"
+import {FamousPeople} from "../../types";
+
+const famousPeople:FamousPeople[] = [
+  {
+    name:"Alfred Adler"
+  },
+  {
+    name:"Necip Fazıl Kısakürek"
+  },
+  {
+    name:"Marcus Aurelius"
+  },
+  {
+    name:"Arthur Schopenhauer"
+  },
+  {
+    name:"Friedrich Nietzsche"
+  },
+  {
+    name:"Napoleon Bonaparte"
+  },
+  {
+    name:"İbn Haldun"
+  },
+  {
+    name:"Cristiano Ronaldo"
+  },
+  {
+    name:"Muammar al-Gaddafi"
+  },
+  {
+    name:"Imam Sharawi"
+  },
+  {
+    name:"Sagopa Kajmer",
+    aliases:["dj mic check","yunus özyavuz"]
+  },
 ]
 export default famousPeople;
