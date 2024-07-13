a3b4d92b97de3053d74dd3eb8ed5577769909f88
Mehmet Emin <mteminn@gmail.com>
2024-07-13 14:46:52 +0300

    convert categories from string array to object array (added category description)

--- src/app/mockDB/categories.ts
+++ src/app/mockDB/categories.ts
@@ -1,13 +1,35 @@
-const categories:string[] = [
-  "movies",
-  "books",
-  "animes",
-  "music",
-  "people",
-  "cartoon&animation",
-  "idiom",
-  "proverb",
+import Category from '../../types';
 
+// @ts-ignore
+const categories:Category[] = [
+  {
+    categoryTitle:"books",
+    categoryDescription: "Passages from books."
+  },
+  {
+    categoryTitle:"people",
+    categoryDescription: "Famous people like authors, leaders"
+  },
+  {
+    categoryTitle:"music",
+    categoryDescription: "Quotes from songs"
+  },
+  {
+    categoryTitle:"proverb",
+    categoryDescription: "Citations by previous generations"
+  },
+  {
+    categoryTitle:"movies",
+    categoryDescription: "Excerpts from movies/fictional characters"
+  },
+  {
+    categoryTitle:"animes",
+    categoryDescription: "Quotations by fictional anime characters"
+  },
+  {
+    categoryTitle:"cartoons & animations",
+    categoryDescription: "Quotations by fictional cartoon characters."
+  },
 ]
 
 export default categories;
