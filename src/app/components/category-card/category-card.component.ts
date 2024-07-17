748e1c3de2ebc967dbd82ec07627f84268cdbd10
Mehmet Emin <mteminn@gmail.com>
2024-07-17 10:27:41 +0300

    created category card component

---
+++ src/app/components/category-card/category-card.component.ts
@@ -0,0 +1,22 @@
+import {Component, ElementRef, Input, signal, WritableSignal} from '@angular/core';
+import {NgClass, NgStyle} from "@angular/common";
+
+@Component({
+  selector: 'app-category-card',
+  standalone: true,
+  imports: [
+    NgStyle,
+    NgClass
+  ],
+  templateUrl: './category-card.component.html',
+  styleUrl: './category-card.component.scss',
+  host: {'class': 'category-card-component'}
+})
+export class CategoryCardComponent {
+
+  @Input({required:true}) categoryTitle!: string;
+  @Input({required:true}) categoryDescription?: string;
+  constructor(private element: ElementRef) {
+  }
+
+}
