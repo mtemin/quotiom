05b95082d79311e3ab93c165d753ee0ae00e8b1f
Mehmet Emin <mteminn@gmail.com>
2024-07-18 13:41:47 +0300

    added Quote type

---
+++ src/app/models/Quote.ts
@@ -0,0 +1,9 @@
+export type Quote = {
+  id: number,
+  language:'tr' | 'en';
+  text: string,
+  author: string,
+  source?: string | string[],
+  categories?: string | string[],
+  likes?: number,
+}
