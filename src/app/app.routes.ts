9c756c4b7786d684d1c4558ce36ee9d30bed7a14
Mehmet Emin <mteminn@gmail.com>
2024-07-19 17:42:09 +0300

    added pagenotfound, quotes and quote pages to routes, fix page title typo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,33 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
+import {HomeComponent} from './pages/home/home.component';
 import {CategoriesComponent} from "./pages/categories/categories.component";
+import {QuoteListComponent} from "./pages/quote-list/quote-list.component";
+import {PageNotFoundComponent} from "./pages/page-not-found/page-not-found.component";
+import {QuoteDetailComponent} from "./pages/quote-list/quote-detail/quote-detail.component";
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
-    title: 'Quotium | Home Page'
+    title: 'Quotiom | Home Page'
   },
   {
     path: 'categories',
     component: CategoriesComponent,
     title: 'Quote Categories'
   },
+  {
+    path: 'quotes',
+    component: QuoteListComponent,
+    title: 'Quotes'
+  },
+  {
+    path: 'quotes/:id',
+    component: QuoteDetailComponent,
+    title: 'Quotes'
+  },
+  { path: '**',
+    component: PageNotFoundComponent,
+    title:'Page not found'
+  },  // Wildcard route for a 404 page
 ];
