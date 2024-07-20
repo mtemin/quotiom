59448af11298c6c4595c6db83394c6cc3ef3c6b1
Mehmet Emin <mteminn@gmail.com>
2024-07-20 15:19:28 +0300

    added viewencapsulation.none to ts file, styled the css to be responsive, and looped with the quotes data in html file

--- src/app/pages/quote-list/quote-list.component.ts
+++ src/app/pages/quote-list/quote-list.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 import {NavbarComponent} from "../../components/navbar/navbar.component";
 import {CategoryCardComponent} from "../../components/category-card/category-card.component";
 import {NgForOf} from "@angular/common";
 import {QuoteComponent} from "../../components/quote/quote.component";
 import {CategoriesSidebarComponent} from "../../components/categories-sidebar/categories-sidebar.component";
+import {quotes} from "../../mockDB/quotes";
 
 @Component({
   selector: 'app-quote-list',
@@ -17,8 +18,11 @@ import {CategoriesSidebarComponent} from "../../components/categories-sidebar/ca
     CategoriesSidebarComponent
   ],
   templateUrl: './quote-list.component.html',
-  styleUrl: './quote-list.component.scss'
+  styleUrl: './quote-list.component.scss',
+  encapsulation: ViewEncapsulation.None
+
 })
 export class QuoteListComponent {
 
+  protected readonly quotes = quotes;
 }
