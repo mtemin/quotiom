e4a6ed29c875fdc35667492c2be435983a5921d8
Mehmet Emin <mteminn@gmail.com>
2024-07-22 12:21:46 +0300

    added books data

---
+++ src/app/mockDB/books.ts
@@ -0,0 +1,43 @@
+import {Book} from "../models/Book";
+
+const books:Book[] = [
+  {
+    id:0,
+    name:"yolların sonu",
+    author:"hüseyin nihal atsız",
+    language:"tr"
+  },
+  {
+    id:1,
+    name:"romeo ve juliet",
+    author:"william shakespeare",
+    language:"tr"
+  },
+  {
+    id:2,
+    name:"çile",
+    author:"necip fazıl kısakürek",
+    language:"tr"
+  },
+  {
+    id:3,
+    name:"yaşama sanatı",
+    author:"alfred adler",
+    language:"tr"
+  },
+  {
+    id:4,
+    name:"böyle buyurdu zerdüşt",
+    author:"friedrich nietzsche",
+    language:"tr"
+  },
+  {
+    id:5,
+    name:" ",
+    author:" ",
+    language:"tr"
+  },
+
+]
+
+export default books;
