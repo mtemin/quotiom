1be0c0c8a2946b873b72ec7cd23d7172bcdf98cd
Mehmet Emin <mteminn@gmail.com>
2024-09-08 19:38:07 +0300

    add description to quote model

--- src/app/models/Quote.ts
+++ src/app/models/Quote.ts
@@ -2,6 +2,7 @@ export type Quote = {
   id: number,
   language:'tr' | 'en';
   text: string,
+  description?:string,
   author: string,
   source?: string | string[],
   categories?: string | string[],
